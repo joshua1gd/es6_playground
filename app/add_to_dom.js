2e39e1118ee5900cc6d8d961db3695d077e3d9c0
Joshua1gd <joshua1gd@yahoo.com>
2019-02-06 16:15:17 -0800

    intro to export and import

---
+++ app/add_to_dom.js
@@ -0,0 +1,8 @@
+
+export default function (tagName, text){
+    const newTag = document.createElement(tagName);
+    newTag.innerText = text;
+
+    document.getElementById('root').appendChild(newTag);
+}
+
--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import addToDom from './add_to_dom';
+import calcDefault, {add, myArray} from './calculate';
+
+console.log(myArray);
+
+calcDefault();
+
+addToDom('h1', `2+8=${add(2,8)}`);
+
